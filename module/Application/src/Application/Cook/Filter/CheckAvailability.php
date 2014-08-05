2c4e51a39edb11e7d468d10f4c7d3809e412eb75
freemany <freemany@freemany.com>
2014-08-05 12:05:06 +1000

    undo traits

--- module/Application/src/Application/Cook/Filter/CheckAvailability.php
+++ module/Application/src/Application/Cook/Filter/CheckAvailability.php
@@ -5,7 +5,24 @@ use DateTime;
 
 class CheckAvailability implements FilterInterface
 {
-    use FilterAwareTrait;
+    protected $fridge;
+    protected $recipes;
+
+    public function setFridge($fridge) {
+        $this->fridge = $fridge;
+    }
+
+    public function getFridge() {
+        return $this->fridge;
+    }
+
+    public function setRecipes($recipes) {
+        $this->recipes = $recipes;
+    }
+
+    public function getRecipes() {
+        return $this->recipes;
+    }
 
     protected $today;
 
